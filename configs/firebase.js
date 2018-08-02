2e5e89431ff41f24534743a102503167f62877fc
Alyssa <amcdevitt@andculture.com>
2018-08-02 09:30:00 -0400

    Added basic firebase and some redux

---
+++ configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC4vulNVtxkNSL_H8vs0PWiQTMTaLmfyLM",
+    authDomain: "testchatapp-7f929.firebaseapp.com",
+    databaseURL: "https://testchatapp-7f929.firebaseio.com",
+    projectId: "testchatapp-7f929",
+    storageBucket: "testchatapp-7f929.appspot.com",
+    messagingSenderId: "802920617164"
+};
